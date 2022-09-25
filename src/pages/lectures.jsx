d2fd061de7f473d41973d5c5cb3feb9512084968
chandimavishwajith <vishwajith.9t@gmail.com>
2022-09-25 11:27:05 +0530

    update ui

---
+++ src/pages/lectures.jsx
@@ -0,0 +1,68 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./styles.css";
+
+const Lectures = () => {
+  return (
+    <div className="dashboard-content">
+      {/* <DashboardHeader btnText="New Order" /> */}
+
+      <div className="dashbord-header-container">
+        <Button variant="secondary" size="md">
+          Upload File
+        </Button>
+        <div className="dashbord-header-right"></div>
+      </div>
+      <div className="dashboard-content-container">
+        <div className="dashboard-content-header">
+          <h2>Lectures</h2>
+        </div>
+        <div className="d-grid gap-3">
+          <Button variant="secondary" size="lg">
+            CGV
+          </Button>
+          <Button variant="secondary" size="lg">
+            ABS
+          </Button>
+        </div>
+
+        {/* <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Filling Station Name</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Filling Station Name</Form.Label>
+            <Form.Control type="text" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Label>Fuel type needed</Form.Label>
+
+            <Form.Select aria-label="Default select example">
+              <option disabled selected>Choose Type</option>
+              <option value="1">Petrol 92</option>
+              <option value="2">Petrol 95</option>
+              <option value="3">Diesel</option>
+              <option value="4">Super Diesel</option>
+
+            </Form.Select>
+            
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Volume needed</Form.Label>
+            <Form.Control type="text" placeholder="in Liters" />
+          </Form.Group>
+
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
+        </Form> */}
+      </div>
+    </div>
+  );
+};
+
+export default Lectures;
