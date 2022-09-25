74ddd32b9ff6a3fcdb182b9715f1a37536c55464
rushanJ <rushanthasindu10@gmail.com>
2022-09-25 17:59:04 +0530

    File Upload Done

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,52 +1,115 @@
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
-import Form from "react-bootstrap/Form";
-import DashboardHeader from "../components/DashboardHeader";
-import * as rb from "react-bootstrap";
+import React from 'react'
+import axios from 'axios';
+ 
+class FileUpload extends React.Component{
+ 
+    constructor(){
+        super();
+        this.state = {
+            selectedFile:'',
+            moduleCode: '',
+            moduleName: '',
+            date: '',
+        }
+ 
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleModuleCodeChange = this.handleModuleCodeChange.bind(this);
+        this.handleModuleNameChange = this.handleModuleNameChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+    }
+ 
+    handleInputChange(event) {
+        this.setState({
+            selectedFile: event.target.files[0],
+          })
+    }
 
-import "./styles.css";
-
-const Upload = () => {
+    handleModuleCodeChange(event) {
+      this.setState({
+        moduleCode: event.target.value,
+        })
+  }
   
-  return (
-    <div className="dashboard-content">
-      <div className="dashbord-header-container">
-        <Link className="nav-link" to="/">
-          <Button variant="secondary" size="md">
-            Back
-          </Button>
-        </Link>
-        <div className="dashbord-header-right"></div>
-      </div>
+  handleModuleNameChange(event) {
+    this.setState({
+      moduleName: event.target.value,
+      })
+}
 
-      <div className="dashboard-content-container">
-        <div className="dashboard-content-header">
-          <h2>Upload</h2>
-        </div>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Date</Form.Label>
-            <Form.Control type="text" placeholder="" />
-          </Form.Group>
+handleDateChange(event) {
+  this.setState({
+    date: event.target.value,
+    })
+}
+ 
+    submit(){
+        const data = new FormData() 
+        data.append('file', this.state.selectedFile)
+        data.append('moduleCode', this.state.moduleCode)
+        data.append('moduleName', this.state.moduleName)
+        data.append('date', this.state.date)
+        console.warn(this.state.selectedFile);
+        let url = "http://localhost:8000/api/file/";
+ 
+        axios.post(url, data, { // receive two parameter endpoint url ,form data 
+        })
+        .then(res => { // then print response status
+            console.warn(res);
+        })
+ 
+    }
+ 
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <br /><br />
+ 
+                            <h3 className="text-white">React File Upload Example - Tutsmake.com</h3>
+                            <br />
+                            <div className="form-row">
+                                <div className="form-group col-md
+                                -6">
+                                    <label className="text-white">Module Code</label>
+                                    <input type="text" className="form-control" name="moduleCode" onChange={this.handleModuleCodeChange} />
+                                </div>
+                            </div>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Lecture</Form.Label>
-            <Form.Control type="text" placeholder="" />
-          </Form.Group>
+                            <div className="form-row">
+                                <div className="form-group col-md
+                                -6">
+                                    <label className="text-white">Module Name</label>
+                                    <input type="text" className="form-control" name="moduleName" onChange={this.handleModuleNameChange} />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="form-group col-md
+                                -6">
+                                    <label className="text-white">Date</label>
+                                    <input type="date" className="form-control" name="moduleCode" onChange={this.handleDateChange} />
+                                </div>
+                            </div>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Image</Form.Label>
-            <Form.Control type="file" name="file" />
-          </Form.Group>
-          
-
-          <Button variant="success" type="submit">
-            Upload
-          </Button>
-        </Form>
-      </div>
-    </div>
-  );
-};
-
-export default Upload;
+                            <div className="form-row">
+                                <div className="form-group col-md
+                                -6">
+                                    <label className="text-white">Select File :</label>
+                                    <input type="file" className="form-control" name="upload_file" onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            
+ 
+                            <div className="form-row">
+                                <div className="col-md-6">
+                                    <button type="submit" className="btn btn-dark" onClick={()=>this.submit()}>Save</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        )  
+    }
+}
+ 
+export default FileUpload;
